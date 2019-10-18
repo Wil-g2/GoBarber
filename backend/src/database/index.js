4eca2f2c2543107cf6b0f36d0458ee6898d80f04
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-10-18 00:59:06 -0300

    add User Controller and User Model

---
+++ backend/src/database/index.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+
+const models = [User];;
+
+class Database {
+  constructor(){
+    this.init();
+  }
+
+  init(){
+    this.connection = new Sequelize(databaseConfig);
+
+    //pass connection to all models the application
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
