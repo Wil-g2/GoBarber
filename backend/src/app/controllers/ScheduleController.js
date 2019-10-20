3e53f66ea8a9f392dc828d3cfc244cc0bb1e8390
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-10-19 21:48:25 -0300

    create a Schedule

---
+++ backend/src/app/controllers/ScheduleController.js
@@ -0,0 +1,36 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+
+    // convert date formart
+    const parseDate = parseISO(date);
+
+    const appointment = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.send(appointment);
+  }
+}
+
+export default new ScheduleController();
