57fbbd1bba08f494ee4787641db993cf6ecf0436
Willian Gaudencio de Rezende <wil-g2@hotmail.com>
2019-10-19 21:47:11 -0300

    remove console.log

--- backend/src/app/controllers/FileController.js
+++ backend/src/app/controllers/FileController.js
@@ -2,7 +2,6 @@ import File from '../models/File';
 
 class FileController {
   async store(req, res) {
-    console.log(req.file);
     const { originalname: name, filename: path } = req.file;
 
     const file = await File.create({
